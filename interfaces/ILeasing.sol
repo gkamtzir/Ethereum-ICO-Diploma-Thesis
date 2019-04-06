6af3c0297fd9be947af0f6f9853063453223f445
George Kamtziridis <georgekam96@gmail.com>
2019-04-06 10:48:56 +0300

    Added ILeasing interface for the Leasing contract.

---
+++ interfaces/ILeasing.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.7;
+
+/**
+  * @title ILeasing interface
+  * @notice Provides the basic functionality of the Leasing
+  * contract.
+  */
+interface ILeasing {
+    function createOffer(uint256 numberOfTokens, uint256 price, uint256 duration) external returns(bool);
+    function removeOffer() external returns(bool);
+    function terminateLeasing() external returns(bool);
+    function leaseFrom(address from) external returns(bool);
+}
