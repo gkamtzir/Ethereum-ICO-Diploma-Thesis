8568fdefea1e8affe6b2de5671d391baf307ae5b
George Kamtziridis <georgekam96@gmail.com>
2019-04-23 13:02:50 +0300

    Added webpack configuration.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,27 @@
-const module = angular.module("mySuperAwesomeApp", []);
+// AngularJS dependencies.
+import * as angular from "angular";
+import "angular-route/angular-route.min.js";
 
-angular.element(document).ready(function() {
-  angular.bootstrap(document, ["mySuperAwesomeApp"]);
+// Bootstrap dependencies.
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+const module = angular.module("openHouseAdminPanel", ["ngRoute"]);
+
+module.config(["$routeProvider", "$locationProvider", ($routeProvider: angular.route.IRouteProvider, $locationProvider: angular.ILocationProvider) => {
+    $locationProvider.hashPrefix("");
+    $routeProvider
+        .when("/privateSale", {
+            template: "Private Sale",
+        })
+        .when("/preICOSale", {
+            template: "Pre ICO Sale"
+        })
+        .when("/ICOSale", {
+            template: "ICO Sale"
+        });
+}]);
+
+angular.element(document).ready(() => {
+    angular.bootstrap(document, ["openHouseAdminPanel"]);
 });
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    //mode: 'development', // or production
+    mode: 'production',
+    entry: {
+        starter: './src/app.ts'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {test: /\.jpg$/, use: 'url-loader?mimetype=image/jpg'},
+            { test: /\.tsx?$/, use: 'ts-loader' },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']    
+            }
+        ]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
+        path: path.resolve('./dist/')
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000,
+        overlay: true
+    }
+
+}
