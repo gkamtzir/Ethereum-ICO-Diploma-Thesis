48c02aba093c7ecefa2f3e3eafefd13e8464f754
George Kamtziridis <georgekam96@gmail.com>
2019-05-07 11:15:33 +0300

    Deleted the deprecated addresses and included the new ones.

--- src/services/web3.service.ts
+++ src/services/web3.service.ts
@@ -12,19 +12,18 @@ export default class Web3Service {
 
     public web3: W3.default;
     public tokenContract: any;
-    public privateSaleContract: any;
+    public privateSaleContract: W3.Contract;
 
     constructor(
         public OpenHouseToken: any,
-        public PrivateSale: any
+        public PrivateSale: any,
+        public OpenHouseTokenContractAddress: string,
+        public PrivateSaleContractAddress: string
     ) {
         this.web3 = new Web3(new Web3.providers.HttpProvider("http://83.212.115.201:5555"));
-        
-        let tokenContractaddress = "0x8d0E3b91823d984967319727E926fDD1e8E928C7";
-        let privateSaleContractAddress = "0xd3a3685D5e7D786390ec0eA868B6FBB55C34f692";
 
-        this.tokenContract = new this.web3.eth.Contract(this.OpenHouseToken.abi, tokenContractaddress);
-        this.privateSaleContract = new this.web3.eth.Contract(this.PrivateSale.abi, privateSaleContractAddress);
+        this.tokenContract = new this.web3.eth.Contract(this.OpenHouseToken.abi, this.OpenHouseTokenContractAddress);
+        this.privateSaleContract = new this.web3.eth.Contract(this.PrivateSale.abi, this.PrivateSaleContractAddress);
     }
 
 }
