42114bb51b7c920403c92a75aeb76c8189b9cf29
George Kamtziridis <georgekam96@gmail.com>
2019-05-07 19:38:46 +0300

    Added comments.

--- test/helpers/ether.js
+++ test/helpers/ether.js
@@ -1,8 +1,12 @@
 const Web3 = require("web3");
 
 module.exports = {
+    /**
+     * Returns the given wei value to ether.
+     * @param number The value in wei.
+     */
     ether: (number) => {
         const web3 = new Web3(new Web3.providers.HttpProvider("http://83.212.115.201:5555"));
         return new web3.utils.BN(web3.utils.toWei(number.toString(), "ether"));
-  }
+    }
 }
