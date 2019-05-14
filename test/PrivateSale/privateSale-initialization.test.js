562b03244882f727b195c5ad29140b187d03e754
George Kamtziridis <georgekam96@gmail.com>
2019-05-14 10:44:48 +0300

    Incorporated the decimals in the initialization test.

--- test/PrivateSale/privateSale-initialization.test.js
+++ test/PrivateSale/privateSale-initialization.test.js
@@ -1,21 +1,33 @@
 const OpenHouseToken = artifacts.require("./OpenHouseToken.sol");
 const PrivateSale = artifacts.require("./PrivateSale.sol");
 const { basicConfiguration, privateSale } = require("../../config.js");
-const { BigNumber } = require("bignumber.js");
+const { expect } = require('chai');
+const BN = web3.utils.BN;
 
 const { duration } = require("../helpers/increaseTime");
 const { latestTime } = require("../helpers/latestTime");
 const { ether } = require("../helpers/ether");
 
 require('chai')
-  .should();
+    .use(require("chai-bn")(BN))
+    .should();
 
 contract("PrivateSale -> initialize", accounts => {
 
     before(async () => {
         this.token = await OpenHouseToken.new(basicConfiguration.totalSupply);
         
-        this.tokenPrice = new BigNumber(privateSale.tokenPrice);
+        // Helper variables;
+        this.power = new BN(10);
+        this.power = this.power.pow(new BN(basicConfiguration.decimals));
+
+        this.tokenPrice = new BN(privateSale.tokenPrice);
+
+        this.tokensMinCap = new BN(privateSale.tokensMinCap)
+        this.tokensMinCap = this.tokensMinCap.mul(this.power);
+
+        this.tokensMaxCap = new BN(privateSale.tokensMaxCap)
+        this.tokensMaxCap = this.tokensMaxCap.mul(this.power);
 
         this.start = await latestTime();
         this.start += duration.hours(1);
@@ -27,9 +39,10 @@ contract("PrivateSale -> initialize", accounts => {
         
         this.privateSale = await PrivateSale.new(
             this.token.address,
-            ether(this.tokenPrice),
-            privateSale.tokensMinCap,
-            privateSale.tokensMaxCap,
+            this.tokenPrice.toString(),
+            this.tokensMinCap.toString(),
+            this.tokensMaxCap.toString(),
+            basicConfiguration.decimals,
             this.start,
             this.end,
             this.redeemableAfter
@@ -38,54 +51,54 @@ contract("PrivateSale -> initialize", accounts => {
         this.admin = accounts[basicConfiguration.adminAccount];
 
         // Allocate the needed tokens to the Private Sale contract.
-        await this.token.transfer(this.privateSale.address, privateSale.tokensMaxCap, { from: this.admin });
+        await this.token.transfer(this.privateSale.address, this.tokensMaxCap.toString(), { from: this.admin });
     });
 
     describe("Initialization", () => {
 
         it("Should initialize private sale with the correct owner", async () => {
             const owner = await this.privateSale.getOwner();
-            owner.should.be.equal(this.admin);
+            expect(owner).to.be.equal(this.admin);
         });
 
         it("Should initialize private sale with the correct price", async () => {
             const price = await this.privateSale.getTokenPrice();
-            price.toNumber().should.be.equal(ether(this.tokenPrice).toNumber());
+            expect(price).to.be.bignumber.equal(this.tokenPrice);
         });
 
         it("Should initialize the private sale with the correct total supply", async () => {
             const totalSupply = await this.token.balanceOf(this.privateSale.address);
-            totalSupply.toNumber().should.be.equal(privateSale.tokensMaxCap);
+            expect(totalSupply).to.be.bignumber.equal(this.tokensMaxCap);
         });
 
         it("Should initialize private sale with the correct minimum cap", async () => {
             const minCap = await this.privateSale.getTokensMinCap();
-            minCap.toNumber().should.be.equal(privateSale.tokensMinCap);
+            expect(minCap).to.be.bignumber.equal(this.tokensMinCap);
         });
 
         it("Should initialize private sale with the correct maximum cap", async () => {
             const maxCap = await this.privateSale.getTokensMaxCap();
-            maxCap.toNumber().should.be.equal(privateSale.tokensMaxCap);
+            expect(maxCap).to.be.bignumber.equal(this.tokensMaxCap);
         });
 
         it("Should initialize private sale with the zero sold tokens", async () => {
             const soldTokens = await this.privateSale.getTokensSold();
-            soldTokens.toNumber().should.be.equal(0);
+            expect(soldTokens).to.be.bignumber.equal(new BN(0));
         });
 
         it("Should initialize private sale with the correct start date", async () => {
             const start = await this.privateSale.getStartTimestamp();
-            start.toNumber().should.be.equal(this.start);
+            expect(start).to.be.bignumber.equal(new BN(this.start));
         });
 
         it("Should initialize private sale with the correct end date", async () => {
             const end = await this.privateSale.getEndTimestamp();
-            end.toNumber().should.be.equal(this.end);
+            expect(end).to.be.bignumber.equal(new BN(this.end));
         });
 
         it("Should initialize the private sale with the correct redeemableAfter timestamp", async () => {
             const redeemableAfter = await this.privateSale.getRedeemableAfterTimestamp();
-            redeemableAfter.toNumber().should.be.equal(this.redeemableAfter);
+            expect(redeemableAfter).to.be.bignumber.equal(new BN(this.redeemableAfter));
         });
 
     });
