fdcec1132323fcb23b7e976923ab4557a152216d
George Kamtziridis <georgekam96@gmail.com>
2019-07-18 16:54:47 +0300

    Added more descriptive messages.

--- watcher/app.js
+++ watcher/app.js
@@ -18,99 +18,145 @@ ICOSaleContract = new web3.eth.Contract(ICOSale.abi, "0x96CA45E3967cf4Fc83BD2572
 // Token events.
 
 // 'OfferCreated' event.
-tokenContract.events.OfferCreated((error, events) => {
-    console.log(error);
-    console.log(events);
+tokenContract.events.OfferCreated((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;
+    console.log(`${data.from} created an offer for ${data.numberOfTokens} tokens for ${data.duration}`
+        + ` at a price of ${data.price} wei.`);
 });
 
 // 'OfferRemoved' event.
-tokenContract.events.OfferRemoved((error, events) => {
-    console.log(error);
-    console.log(events);
+tokenContract.events.OfferRemoved((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;
+    console.log(`${data.from} removed his offer.`);
 });
 
 // 'Leased' event.
-tokenContract.events.Leased((error, events) => {
-    console.log(error);
-    console.log(events);
+tokenContract.events.Leased((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;
+    console.log(`${data.from} leased from ${data.to}.`);
 });
 
 // 'LeasingTerminated' event.
-tokenContract.events.LeasingTerminated((error, events) => {
-    console.log(error);
-    console.log(events);
+tokenContract.events.LeasingTerminated((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;
+    console.log(`${data.from} terminated the leasing to ${data.to}.`);
 });
 
 // 'CommitedFromBalance' event.
-tokenContract.events.CommitedFromBalance((error, events) => {
-    console.log(error);
-    console.log(events);
+tokenContract.events.CommitedFromBalance((error, event) => {
+    if (error)
+        return;
+
+    const data = event.returnValues;
+    console.log(`${data.from} commited ${data.numberOfTokens} tokens from balance.`);
 });
 
 // 'CommitedFromRented' event.
-tokenContract.events.CommitedFromRented((error, events) => {
-    console.log(error);
-    console.log(events);
+tokenContract.events.CommitedFromRented((error, event) => {
+    if (error)
+        return;
+
+    const data = event.returnValues;
+    console.log(`${data.from} commited ${data.numberOfTokens} tokens from rented.`);
 });
 
 // Private sale events.
 
 // 'Sold' event.
-privateSaleContract.events.Sold((error, events) => {
-    console.log(error);
-    console.log(events);
+privateSaleContract.events.Sold((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;
+    console.log(`${data.from} bought ${data.numberOfTokens} tokens during the private sale.`);
 });
 
 // 'Refunded' event.
-privateSaleContract.events.Refunded((error, events) => {
-    console.log(error);
-    console.log(events);
+privateSaleContract.events.Refunded((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;    
+    console.log(`${data.from} refunded ${data.numberOfTokens} tokens during the private sale.`);
 })
 
 // 'Redeemed' event.
-privateSaleContract.events.Redeemed((error, events) => {
-    console.log(error);
-    console.log(events);
+privateSaleContract.events.Redeemed((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;
+    console.log(`${data.from} redeemed ${data.numberOfTokens} tokens from the private sale.`);
 });
 
 // Pre-ICO sale events.
 
 // 'Sold' event.
-preICOSaleContract.events.Sold((error, events) => {
-    console.log(error);
-    console.log(events);
+preICOSaleContract.events.Sold((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;
+    console.log(`${data.from} bought ${data.numberOfTokens} tokens during the pre-ICO sale.`);
 });
 
 // 'Refunded' event.
-preICOSaleContract.events.Refunded((error, events) => {
-    console.log(error);
-    console.log(events);
+preICOSaleContract.events.Refunded((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;
+    console.log(`${data.from} refunded ${data.numberOfTokens} tokens during the pre-ICO sale.`);
 })
 
 // 'Redeemed' event.
-preICOSaleContract.events.Redeemed((error, events) => {
-    console.log(error);
-    console.log(events);
+preICOSaleContract.events.Redeemed((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;
+    console.log(`${data.from} redeemed ${data.numberOfTokens} tokens from the pre-ICO sale.`);
 });
 
 // ICO sale events.
 
 // 'Sold' event.
-ICOSaleContract.events.Sold((error, events) => {
-    console.log(error);
-    console.log(events);
+ICOSaleContract.events.Sold((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;
+    console.log(`${data.from} bought ${data.numberOfTokens} tokens during the ICO sale.`);
 });
 
 // 'Refunded' event.
-ICOSaleContract.events.Refunded((error, events) => {
-    console.log(error);
-    console.log(events);
+ICOSaleContract.events.Refunded((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;
+    console.log(`${data.from} refunded ${data.numberOfTokens} tokens during the ICO sale.`);
 })
 
 // 'Redeemed' event.
-ICOSaleContract.events.Redeemed((error, events) => {
-    console.log(error);
-    console.log(events);
+ICOSaleContract.events.Redeemed((error, event) => {
+    if (error)
+        return;
+    
+    const data = event.returnValues;
+    console.log(`${data.from} redeemed ${data.numberOfTokens} tokens from the ICO sale.`);
 });
 
 console.log("Watcher is up and running...");
