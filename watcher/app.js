31b3fd31537d1d7fb3468fa562b9f4d3e587492b
George Kamtziridis <georgekam96@gmail.com>
2019-07-19 15:15:48 +0300

    Added 'AddressAllowed' event to watcher.

--- watcher/app.js
+++ watcher/app.js
@@ -10,10 +10,10 @@ const ICOSale = require("../build/contracts/ICOSale.json");
 let web3 = new Web3(new Web3.providers.WebsocketProvider("ws://83.212.115.201:5555"));
 
 // Instantiating contracts.
-tokenContract = new web3.eth.Contract(OpenHouseToken.abi, "0x25312750426b8939a9d184f8DBb7D2C19A791c07");
-privateSaleContract = new web3.eth.Contract(PrivateSale.abi, "0x13f4417BbA1731b0d3ae493Ea754c12b375713a7");
-preICOSaleContract = new web3.eth.Contract(PreICOSale.abi, "0x0975D77D2cEF039692bb1c138d4e14914561F9C1");
-ICOSaleContract = new web3.eth.Contract(ICOSale.abi, "0x96CA45E3967cf4Fc83BD2572819D228f4876b271");
+tokenContract = new web3.eth.Contract(OpenHouseToken.abi, "0xb030CC59d57f871F097C86783034c658EB23a444");
+privateSaleContract = new web3.eth.Contract(PrivateSale.abi, "0x7D294909EA7fB3bd015F1C083AF0445aCFd43c31");
+preICOSaleContract = new web3.eth.Contract(PreICOSale.abi, "0xC8B73D244A62D0857C962F54BFd5955320BCB25a");
+ICOSaleContract = new web3.eth.Contract(ICOSale.abi, "0xe1dDeA8606A1f761d34938C110b9609590785668");
 
 // Token events.
 
@@ -99,6 +99,14 @@ privateSaleContract.events.Redeemed((error, event) => {
     
     const data = event.returnValues;
     console.log(`${data.from} redeemed ${data.numberOfTokens} tokens from the private sale.`);
+});
+
+privateSaleContract.events.AddressAllowed((error, event) => {
+    if (error)
+        return;
+
+    const data = event.returnValues;
+    console.log(`${data.allowed} has been allowed to participate in the private sale.`);
 });
 
 // Pre-ICO sale events.
